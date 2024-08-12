e2616b653123ffc2c3bcaf9e86abd81bec9bcaa1
Thanujak16 <119279936+Thanujak16@users.noreply.github.com>
2024-08-12 15:46:47 +0530

    Create Japanese Cities' Attributes.sql

---
+++ SQL/Japanese Cities' Attributes.sql
@@ -0,0 +1,7 @@
+# Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
+
+# The CITY table is described as follows:
+CITY.jpg
+
+SELECT * FROM CITY
+WHERE COUNTRYCODE = "JPN";
