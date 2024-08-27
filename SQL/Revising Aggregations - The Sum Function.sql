0a4f10e2774b2cc4625d52628f8f29c9d542caad
Thanujak16 <119279936+Thanujak16@users.noreply.github.com>
2024-08-27 14:35:43 +0530

    Create Revising Aggregations - The Sum Function.sql

---
+++ SQL/Revising Aggregations - The Sum Function.sql
@@ -0,0 +1,6 @@
+#Query the total population of all cities in CITY where District is California.
+#Input Format
+#The CITY table is described as follows: CITY.jpg
+
+SELECT SUM(POPULATION)FROM CITY
+WHERE DISTRICT ="California";
