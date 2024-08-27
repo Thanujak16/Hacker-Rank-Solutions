cad204c84c374735a8e14b6732789589a067e4eb
Thanujak16 <119279936+Thanujak16@users.noreply.github.com>
2024-08-27 15:01:57 +0530

    Create Population Density Difference.sql

---
+++ SQL/Population Density Difference.sql
@@ -0,0 +1,5 @@
+#Query the difference between the maximum and minimum populations in CITY.
+#Input Format
+#The CITY table is described as follows: CITY.jpg
+
+  SELECT MAX(POPULATION)-MIN(POPULATION) FROM CITY
