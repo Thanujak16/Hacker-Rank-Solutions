ec3b7952b4775220ce80d6ff5718fb390b7acdbe
Thanujak16 <119279936+Thanujak16@users.noreply.github.com>
2024-08-27 14:32:38 +0530

    Create Revising Aggregations - The Count Function.sql

---
+++ SQL/Revising Aggregations - The Count Function.sql
@@ -0,0 +1,6 @@
+#Query a count of the number of cities in CITY having a Population larger than .
+#Input Format
+#The CITY table is described as follows: CITY.jpg
+
+SELECT COUNT(NAME)FROM CITY
+WHERE POPULATION >100000;
